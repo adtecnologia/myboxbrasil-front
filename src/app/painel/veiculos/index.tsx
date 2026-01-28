be5bda04fd65d75fbc0724e5778781e66261a151
Wendell Nascimento <wendell.vnasc@gmail.com>
2026-01-28 10:09:16 -0300

    fix: correção dash prefeitura

--- src/app/painel/veiculos/index.tsx
+++ src/app/painel/veiculos/index.tsx
@@ -1,64 +1,134 @@
 // react libraries
+
+import { Col, Row } from "antd";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Col, Row } from "antd";
-
-// services
-import { PageDefaultProps } from "../../../services";
+import CardItem from "../../../components/CardItem";
 
 // components
 import PageDefault from "../../../components/PageDefault";
-import CardItem from "../../../components/CardItem";
 import Table from "../../../components/Table";
-import { TableNewButton, TableReturnButton, TableTrEditButton, TableTrRecoverButton, TableTrTrashButton, TableTrashButton } from "../../../components/Table/buttons";
+import {
+  TableNewButton,
+  TableReturnButton,
+  TableTrashButton,
+  TableTrEditButton,
+  TableTrRecoverButton,
+  TableTrTrashButton,
+} from "../../../components/Table/buttons";
+// services
+import type { PageDefaultProps } from "../../../services";
 
 const VehicleList = ({ type, path, permission }: PageDefaultProps) => {
-  
   // state
   const [action, setAction] = useState(false);
 
   // table columns
   const column = [
-    { title: "Placa", dataIndex: "plate", table: "plate", width: "160px", sorter: true, align: "center", render: null },
-    { title: "Renavam", dataIndex: "renavam", table: "renavam", width: "180px", sorter: true, align: "center", render: null },
-    { title: "Marca", dataIndex: "brand", table: "brand", width: "180px", sorter: true, align: "center", render: null },
-    { title: "Modelo", dataIndex: "model", table: "model", width: "200px", sorter: true, align: "center", render: null },
-    { title: "Tipo de Veículo", dataIndex: "vehicle_type.name", table: "vehicle_types.name", width: "auto", minWidth: "200px", sorter: true, align: "left", render: null },
-    { title: "Ações", dataIndex: null, width: "100px", sorter: false, align: "center", render: (item: any) => (
-      <Row justify={"center"} style={{ width: "100%" }}>
-        <TableTrEditButton type={type} permission={permission} item={item} />
-        <TableTrTrashButton type={type} permission={permission} item={item} action={() => setAction(!action)} path={path}/>
-        <TableTrRecoverButton type={type} permission={permission} item={item} action={() => setAction(!action)} path={path}/>
-      </Row>
-    )},
+    {
+      title: "Placa",
+      dataIndex: "plate",
+      table: "plate",
+      width: "160px",
+      sorter: true,
+      align: "center",
+      render: null,
+    },
+    {
+      title: "Renavam",
+      dataIndex: "renavam",
+      table: "renavam",
+      width: "180px",
+      sorter: true,
+      align: "center",
+      render: null,
+    },
+    {
+      title: "Marca",
+      dataIndex: "brand",
+      table: "brand",
+      width: "180px",
+      sorter: true,
+      align: "center",
+      render: null,
+    },
+    {
+      title: "Modelo",
+      dataIndex: "model",
+      table: "model",
+      width: "200px",
+      sorter: true,
+      align: "center",
+      render: null,
+    },
+    {
+      title: "Tipo de Veículo",
+      dataIndex: "vehicle_type.name",
+      table: "vehicle_types.name",
+      width: "auto",
+      minWidth: "200px",
+      sorter: true,
+      align: "left",
+      render: null,
+    },
+    {
+      title: "Ações",
+      dataIndex: null,
+      width: "100px",
+      sorter: false,
+      align: "center",
+      render: (item: any) => (
+        <Row justify={"center"} style={{ width: "100%" }}>
+          <TableTrEditButton item={item} permission={permission} type={type} />
+          <TableTrTrashButton
+            action={() => setAction(!action)}
+            item={item}
+            path={path}
+            permission={permission}
+            type={type}
+          />
+          <TableTrRecoverButton
+            action={() => setAction(!action)}
+            item={item}
+            path={path}
+            permission={permission}
+            type={type}
+          />
+        </Row>
+      ),
+    },
   ];
 
   return (
-    <PageDefault valid={`${permission}.${type}`} items={[
+    <PageDefault
+      items={[
         { title: <Link to={type === "list" ? "#" : ".."}>Veículos</Link> },
         { title: type === "list" ? "Lista" : "Lixeira" },
-    ]} options={
-      <Row justify={"end"} gutter={[8, 8]}>
-        <TableNewButton type={type} permission={permission} />
-        <TableTrashButton type={type} permission={permission} />
-        <TableReturnButton type={type} permission={permission} />
-      </Row>
-    }>
+      ]}
+      options={
+        <Row gutter={[8, 8]} justify={"end"}>
+          <TableNewButton permission={permission} type={type} />
+          <TableTrashButton permission={permission} type={type} />
+          <TableReturnButton permission={permission} type={type} />
+        </Row>
+      }
+      valid={`${permission}.${type}`}
+    >
       <Row gutter={[16, 16]}>
         <Col md={24} xs={24}>
           <CardItem>
             <Table
+              action={action}
               column={column}
               path={path}
               type={type}
-              action={action}
               useFilter={[
                 {
                   type: "search",
                   name: "vehicle_type_id",
                   label: "Tipo de Veículo",
                   url: "/vehicle_type",
-                  labelField: ["id", "name"]
+                  labelField: ["id", "name"],
                 },
               ]}
             />
--- src/components/PageDefault/index.tsx
+++ src/components/PageDefault/index.tsx
@@ -1,43 +1,54 @@
 // BIBLIOTECAS REACT
 import { Breadcrumb, Col, Row } from "antd";
 
-// ICONES
-import { RiHome3Line } from "react-icons/ri";
-
 // CSS
-import './styles.css'; 
+import "./styles.css";
 import { Link } from "react-router-dom";
 import { verifyConfig } from "../../services";
 import LoadItem from "../LoadItem";
 
 // INTERFACE
 interface PageDefaultInterface {
-    children?: any,
-    items: any[],
-    options?: any,
-    valid: any
+  children?: any;
+  items: any[];
+  options?: any;
+  valid: any;
 }
 
-const PageDefault = ( { children, items, options, valid } : PageDefaultInterface ) => {
-
-    if (verifyConfig(valid)) {
-        return (
-            <Row>
-                <Col span={24} className="page-default-padding">
-                    <Row justify={'space-between'} align={'middle'}>
-                        <Col><Breadcrumb className="page-default-breadcrumb" items={[ { title: <Link to="/painel/dashboard">Painel</Link> }, ...items]} /></Col>
-                        <Col>{options}</Col>
-                    </Row>
-                </Col>
-                <Col span={24} style={{marginTop: 16}} className="page-default-page page-default-padding">
-                    {children}
-                </Col>
-            </Row>
-        )
-    } else {
-        return <LoadItem title="Você não tem permissão para acessar essa página"/>
-    }
-
-}
+const PageDefault = ({
+  children,
+  items,
+  options,
+  valid,
+}: PageDefaultInterface) => {
+  if (verifyConfig(valid)) {
+    return (
+      <Row>
+        <Col className="page-default-padding" span={24}>
+          <Row align={"middle"} justify={"space-between"}>
+            <Col>
+              <Breadcrumb
+                className="page-default-breadcrumb"
+                items={[
+                  { title: <Link to="/painel/dashboard">Painel</Link> },
+                  ...items,
+                ]}
+              />
+            </Col>
+            <Col>{options}</Col>
+          </Row>
+        </Col>
+        <Col
+          className="page-default-page page-default-padding"
+          span={24}
+          style={{ marginTop: 16 }}
+        >
+          {children}
+        </Col>
+      </Row>
+    );
+  }
+  return <LoadItem title="Você não tem permissão para acessar essa página" />;
+};
 
 export default PageDefault;
